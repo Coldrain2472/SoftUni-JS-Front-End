0614a312fe04b7e81f53e5fe5bed98f57498fa21
Anelia Peycheva <anelia.peicheva@gmail.com>
2025-03-06 12:57:06 +0200

    Create 06.ModernTimesOfHashTag.js

---
+++ 02. JS Arrays & Strings/02. Exercise/06.ModernTimesOfHashTag.js
@@ -0,0 +1,11 @@
+function modernTimesOfHashtag(text) {
+    let regex = /#\b[a-zA-Z]+\b/g;
+
+    let validWords = text.match(regex);
+
+    for (word of validWords) {
+        console.log(word.substring(1));
+    }
+}
+
+modernTimesOfHashtag('Nowadays everyone uses # to tag a #special word in #socialMedia');
