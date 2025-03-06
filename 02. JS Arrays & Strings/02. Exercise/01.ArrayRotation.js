fe890c078301bc5580f47a8993a7b1071d17e9bd
Anelia Peycheva <anelia.peicheva@gmail.com>
2025-03-06 12:56:56 +0200

    Update 01.ArrayRotation.js

---
+++ 02. JS Arrays & Strings/02. Exercise/01.ArrayRotation.js
@@ -0,0 +1,10 @@
+function arrayRotation(array, rotationsCount) {
+    for (let i = 1; i <= rotationsCount; i++) {
+        let firstElement = array.shift();
+        array.push(firstElement);
+    }
+
+    console.log(array.join(' '));
+}
+
+arrayRotation([51, 47, 32, 61, 21], 2);
