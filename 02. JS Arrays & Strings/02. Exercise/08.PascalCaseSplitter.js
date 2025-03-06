e55bf21d2db7a983e52e8ec522ffa9a89c7e4947
Anelia Peycheva <anelia.peicheva@gmail.com>
2025-03-06 12:57:10 +0200

    Create 08.PascalCaseSplitter.js

---
+++ 02. JS Arrays & Strings/02. Exercise/08.PascalCaseSplitter.js
@@ -0,0 +1,8 @@
+function pascalCaseSplitter(text) {
+    let words = text.split(/(?=[A-Z])/);
+    let result = words.join(', ');
+
+    console.log(result);
+}
+
+pascalCaseSplitter('SplitMeIfYouCanHaHaYouCantOrYouCan');
