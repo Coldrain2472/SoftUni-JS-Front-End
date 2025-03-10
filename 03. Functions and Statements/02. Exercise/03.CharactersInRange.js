ff1f8a7d223c2b23cf5388ce4178e643252c1eee
Anelia Peycheva <anelia.peicheva@gmail.com>
2025-03-10 12:31:04 +0200

    Update 03.CharactersInRange.js

---
+++ 03. Functions and Statements/02. Exercise/03.CharactersInRange.js
@@ -0,0 +1,15 @@
+function charactersInRange(charA, charB) {
+    const charANumber = charA.charCodeAt();
+    const charBNumber = charB.charCodeAt();
+    let result = [];
+
+    for (let i = Math.min(charANumber, charBNumber) + 1; i < Math.max(charANumber, charBNumber); i++) {
+        result.push(String.fromCharCode(i));
+    }
+
+    console.log(result.join(' '));
+}
+
+charactersInRange('a', 'd');
+charactersInRange('#', ':');
+charactersInRange('C', '#');
