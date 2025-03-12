e76dbad9767167eeddd5a43c36f58d371da47def
Anelia Peycheva <anelia.peicheva@gmail.com>
2025-03-12 13:02:16 +0200

    Update 02.Towns.js

---
+++ 04. Objects and Classes/02. Exercise/02.Towns.js
@@ -0,0 +1,16 @@
+function towns (townsList) {
+    for (let i = 0; i < townsList.length; i++) {
+        let [town, latitude, longitude] = townsList[i].split(' | ');
+        
+        let currentTownInfo = {
+			town: town,
+			latitude: Number(latitude).toFixed(2),
+			longitude: Number(longitude).toFixed(2),
+		};
+		console.log(currentTownInfo);   
+    };
+}
+
+towns(['Sofia | 42.696552 | 23.32601',
+    'Beijing | 39.913818 | 116.363625']
+    );
