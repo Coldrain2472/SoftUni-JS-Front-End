1268aa918e7db3c43aaf5f23b589421f1bde51dd
Anelia Peycheva <anelia.peicheva@gmail.com>
2025-03-12 13:02:20 +0200

    Update 04.Movies.js

---
+++ 04. Objects and Classes/02. Exercise/04.Movies.js
@@ -0,0 +1,56 @@
+function movies (commandsList) {
+
+    class Movie {
+        constructor(name, director, date){
+            this.name = name,
+            this.director = director
+            this.date = date
+        }
+    }
+
+    let movies = [];
+
+    for (let i = 0; i < commandsList.length; i++) {
+
+        if (commandsList[i].includes('addMovie')) {
+            let info = commandsList[i].split('addMovie ');
+            let movieName = info[1];
+            movies.push(new Movie(movieName, null, null));
+
+        } else if (commandsList[i].includes('directedBy')) {
+            let info = commandsList[i].split(' directedBy ');
+            let movieName = info[0];
+            let directorName = info[1];
+
+            let movie = movies.find(m => m.name === movieName);
+            if (movie) {
+                movie.director = directorName;
+            }
+
+        } else if (commandsList[i].includes('onDate')) {
+            let info = commandsList[i].split(' onDate ');
+            let movieName = info[0];
+            let date = info[1];
+
+            let movie = movies.find(m => m.name === movieName);
+            if (movie) {
+                movie.date = date;
+            }
+        }
+    }
+
+    movies.filter(movie => movie.director && movie.date)
+        .forEach(movie =>console.log(JSON.stringify(movie)));
+}
+
+movies([
+    'addMovie Fast and Furious',
+    'addMovie Godfather',
+    'Inception directedBy Christopher Nolan',
+    'Godfather directedBy Francis Ford Coppola',
+    'Godfather onDate 29.07.2018',
+    'Fast and Furious onDate 30.07.2018',
+    'Batman onDate 01.08.2018',
+    'Fast and Furious directedBy Rob Cohen'
+    ]
+    );
