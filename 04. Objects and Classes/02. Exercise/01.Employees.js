fc41ad68438c665b63f46160180d1f39f660412b
Anelia Peycheva <anelia.peicheva@gmail.com>
2025-03-12 13:02:14 +0200

    Update 01.Employees.js

---
+++ 04. Objects and Classes/02. Exercise/01.Employees.js
@@ -0,0 +1,27 @@
+function employees (namesList) {
+
+    class Employee {
+        constructor(name, personalNumber){
+            this.name = name,
+            this.personalNumber = personalNumber
+        };
+    };
+
+    let employees = [];
+
+    for (let i = 0; i < namesList.length; i++) {
+        employees.push(new Employee(namesList[i], namesList[i].length));
+    };
+
+    for (employee of employees) {
+        console.log(`Name: ${employee.name} -- Personal Number: ${employee.personalNumber}`);
+    };
+}
+
+employees([
+    'Silas Butler',
+    'Adnaan Buckley',
+    'Juan Peterson',
+    'Brendan Villarreal'
+    ]
+    );
