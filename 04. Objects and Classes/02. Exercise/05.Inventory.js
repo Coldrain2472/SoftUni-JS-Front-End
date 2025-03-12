d5a6761707e507873aa9b4361b3805d4fa86adea
Anelia Peycheva <anelia.peicheva@gmail.com>
2025-03-12 13:02:22 +0200

    Update 05.Inventory.js

---
+++ 04. Objects and Classes/02. Exercise/05.Inventory.js
@@ -0,0 +1,32 @@
+function inventory(input) {
+    let heroes = [];
+
+    for (let line of input) {
+        let [heroName, heroLevel, itemsString] = line.split(' / ');
+        heroLevel = Number(heroLevel);
+        let items = itemsString ? itemsString.split(', ') : [];
+
+        let hero = {
+            name: heroName,
+            level: heroLevel,
+            items: items
+        };
+
+        heroes.push(hero);
+    }
+
+    heroes.sort((a, b) => a.level - b.level);
+
+    for (let hero of heroes) {
+        console.log(`Hero: ${hero.name}`);
+        console.log(`level => ${hero.level}`);
+        console.log(`items => ${hero.items.join(', ')}`);
+    }
+}
+
+inventory([
+    'Isacc / 25 / Apple, GravityGun',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara'
+    ]
+    );
