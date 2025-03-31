7022848ab98124340bb351ff91b0381c02aafdc6
Anelia Peycheva <anelia.peicheva@gmail.com>
2025-03-31 14:11:01 +0300

    Create dynamic-validation.js

---
+++ 06. DOM Manipulation and Events/01. Lab/06. Dynamic Validation/dynamic-validation.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', solve);
+
+function solve() {
+    document.getElementById('email').addEventListener('change', onChange);
+
+    function onChange({target}){
+        let pattern = /^[a-z]+@[a-z]+\.[a-z]+$/;
+
+        if(pattern.test(target.value)){
+            target.classList.remove('error');
+        }else{
+            target.classList.add('error');
+        }
+    }
+}
