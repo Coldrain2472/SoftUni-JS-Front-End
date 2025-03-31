8e56d1d7ec204d661ccd7c0823e8323f0c0981d6
Anelia Peycheva <anelia.peicheva@gmail.com>
2025-03-31 14:10:53 +0300

    Create hightlight-active.js

---
+++ 06. DOM Manipulation and Events/01. Lab/05. Highlight Active/hightlight-active.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', focused);
+
+function focused() {
+    let inputs = document.querySelectorAll('input')
+
+    for(const input of inputs){
+        input.addEventListener('focus', focus)
+        input.addEventListener('blur', blur)
+    }
+    
+    function focus(element){
+        element.currentTarget.parentElement.classList.add('focused')
+    }
+    function blur(element){
+        element.currentTarget.parentElement.classList.remove('focused')
+    }
+}
