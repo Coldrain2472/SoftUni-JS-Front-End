20c30528a2cc4ad9a4536dfe4ddf71030acdd1c5
Anelia Peycheva <anelia.peicheva@gmail.com>
2025-03-31 13:39:15 +0300

    Create delete-from-table.js

---
+++ 06. DOM Manipulation and Events/01. Lab/02. Delete from Table/delete-from-table.js
@@ -0,0 +1,26 @@
+function deleteByEmail() {
+    let input = document.querySelector('[name="email"]');
+    let pattern = input.value;
+
+    let rows = Array.from(document.querySelectorAll('tbody tr'));
+
+    let deleted = false;
+
+    for (let row of rows) {
+        let emailCol = row.children[1];
+        let email = emailCol.textContent;
+
+        if (email.includes(pattern)) {
+            row.remove();
+            deleted = true;
+        }
+    }
+
+    let output = document.getElementById('result');
+
+    if (deleted) {
+        output.textContent = 'Deleted.';
+    } else {
+        output.textContent = 'Not found.';
+    }
+}
