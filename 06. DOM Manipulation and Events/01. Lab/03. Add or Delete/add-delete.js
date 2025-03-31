b2ed323ad3424a6a8a8f18684e9f569a3bbb7bf4
Anelia Peycheva <anelia.peicheva@gmail.com>
2025-03-31 14:10:37 +0300

    Update add-delete.js

--- 06. DOM Manipulation and Events/01. Lab/03. Add or Delete/add-delete.js
+++ 06. DOM Manipulation and Events/01. Lab/03. Add or Delete/add-delete.js
@@ -1,3 +1,17 @@
 function addItem() {
-    // TODO
+    let itemsElement = document.getElementById('items');
+    let newItemTextElement = document.getElementById('newItemText');
+
+    let liElement = document.createElement('li');
+    liElement.textContent = newItemTextElement.value;
+
+    let deleteButton = document.createElement('a');
+    deleteButton.textContent = '[Delete]';
+    deleteButton.setAttribute('href', '#');
+    deleteButton.addEventListener('click', (e) => {
+        e.currentTarget.parentElement.remove();
+    });
+
+    liElement.appendChild(deleteButton);
+    itemsElement.appendChild(liElement);
 }
