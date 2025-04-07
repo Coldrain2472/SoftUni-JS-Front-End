4d0edd4a8507e7444b1bfde158d792a289153e55
Anelia Peycheva <anelia.peicheva@gmail.com>
2025-04-07 13:38:21 +0300

    Update app.js

--- 07. Asynchronous Programming/02. Bus Schedule/app.js
+++ 07. Asynchronous Programming/02. Bus Schedule/app.js
@@ -1,17 +1,38 @@
 function solve() {
+    const textBox = document.querySelector('#info span')
+    const departBtn = document.querySelector('#depart')
+    const arriveBtn = document.querySelector('#arrive')
 
-    function depart() {
-        console.log('Depart TODO...');
+    const stopInfo = {
+        name: 'Depot',
+        next: 'depot'
     }
 
-    function arrive() {
-        console.log('Arrive TODO...');
+    async function depart() {
+
+        changeBtnStatus()
+
+        const nextStop = await ((await fetch(`http://localhost:3030/jsonstore/bus/schedule/${stopInfo.next}`)).json())
+        stopInfo.name = nextStop.name
+        stopInfo.next = nextStop.next
+        textBox.textContent = `Next stop ${stopInfo.name}`
+
+    }
+
+    async function arrive() {
+        textBox.textContent = `Arraving at ${stopInfo.name}`
+        changeBtnStatus()
     }
 
     return {
         depart,
         arrive
     };
+
+    function changeBtnStatus() {
+        departBtn.disabled = !departBtn.disabled
+        arriveBtn.disabled = !arriveBtn.disabled
+    }
 }
 
 let result = solve();
